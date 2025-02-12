d516d07a50174b16321e4f31cd647418a9845187
Kirill Beltchikov <bkm.front@yandex.ru>
2025-02-13 00:33:46 +0300

    Исправлена функция создания карточки, исправлены пути к DOM-элементам, исправлена функция удаления карточки, исправлен вывод карточек, исправлены переменные, убран лишний код

--- scripts/index.js
+++ scripts/index.js
@@ -2,69 +2,36 @@
 const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
-const addCardButton = document.querySelector('body > div > main > section.profile.page__section > button');
-const popupCard = document.querySelector('body > div > div.popup.popup_type_new-card');
-const popupImage = document.querySelector('body > div > div.popup.popup_type_image > div.popup__content_content_image');
+
+const cardList = document.querySelector('.places__list');
+const popupImage = document.querySelector('.popup__content_content_image');
 const popupCardImage = popupImage.querySelector('.popup__image');
-const popupForm = popupCard.querySelector('.popup__form');
+const addCardButton = document.querySelector('.profile__add-button');
+const popupCard = document.querySelector('.popup_type_new-card');
 const savePopupCardButton = popupCard.querySelector('.popup__button');
-const cardList = document.querySelector('ul.places__list');
+const popupForm = popupCard.querySelector('.popup__form');
 const placeName = popupCard.querySelector('.popup__input_type_card-name');
 const link = popupCard.querySelector('.popup__input_type_url');
 
 // @todo: Функция создания карточки
-function placeCards(obj) {
+function createCard(cardData) {
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
     const cardImage = cardElement.querySelector('.card__image');
     const cardTitle = cardElement.querySelector('.card__title');
-        cardImage.src = obj.link;
-        cardTitle.textContent = obj.name;
-        cardList.append(cardElement);
-    const deleteButton = cardList.querySelectorAll('.card__delete-button');
-    deleteButton.forEach((elem) => {
-        elem.addEventListener('click', deleteCard);
-    });  
-}
-
-function openPopup() {
-    popupCard.classList.add('popup_is-opened');
-    const popupCloseButton = document.querySelector('body > div > div.popup.popup_type_new-card.popup_is-opened > div > button');
-    popupCloseButton.addEventListener('click', closePopup);
-}
-
-function closePopup() {
-    popupForm.reset();
-    popupCard.classList.remove('popup_is-opened');
+        cardImage.src = cardData.link;
+        cardTitle.textContent = cardData.name;
+        const deleteButton = cardElement.querySelector('.card__delete-button');
+            deleteButton.addEventListener('click', deleteCard);
+    return cardElement;
 }
 
 // @todo: Функция удаления карточки
-function deleteCard() {
-    const card = this.parentElement;
-    card.remove();
-}
+function deleteCard (event) { 
+    event.target.closest(".card").remove(); 
+};
 
 // @todo: Вывести карточки на страницу
-let newInitialCards = initialCards;
-
-function renderCardsFromArray() {
-    newInitialCards.forEach((element, index, arr) => {
-        placeCards(arr[index]);
-    });
-}
-
-function renderNewCard() {
-    const cardsArrayLength = newInitialCards.length - 1;
-    placeCards(newInitialCards[cardsArrayLength]);
-}
-
-addCardButton.addEventListener('click', openPopup);
-
-savePopupCardButton.addEventListener('click', (resetInputs) => {
-    const cardObj = {name: placeName.value, link: link.value,}
-    newInitialCards.push(cardObj);
-    resetInputs.preventDefault();
-    closePopup();
-    renderNewCard();
+initialCards.forEach((cardData) => {
+    const cardElement = createCard(cardData);
+    cardList.append(cardElement);
 });
-
-renderCardsFromArray();

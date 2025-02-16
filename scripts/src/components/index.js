7f4a6bc8dc5ce575f5eeec6110ce6998b6a36113
Kirill Beltchikov <bkm.front@yandex.ru>
2025-02-16 12:35:22 +0300

    Функционал открытия и закрытия модальных окон профиля и новой карточки, закрытие по оверлею, кнопки и клавиши Escape

--- scripts/src/components/index.js
+++ scripts/src/components/index.js
@@ -1,5 +1,5 @@
 import { initialCards, createCard, deleteCard } from './cards.js'
-import {openModal, closeModal} from './modal.js'
+import {openModal, closeModal, closePopupOnBackground} from './modal.js'
 
 // @todo: Темплейт карточки
 export const cardTemplate = document.querySelector('#card-template').content;
@@ -8,31 +8,58 @@ export const cardTemplate = document.querySelector('#card-template').content;
 const cardList = document.querySelector('.places__list');
 const popupImage = document.querySelector('.popup__content_content_image');
 const popupCardImage = popupImage.querySelector('.popup__image');
-const profileAddButton = document.querySelector('.profile__add-button');
+
 const popupCard = document.querySelector('.popup_type_new-card');
+const profileAddButton = document.querySelector('.profile__add-button');
 const closePopupButton = popupCard.querySelector('.popup__close');
 const savePopupCardButton = popupCard.querySelector('.popup__button');
 const modalForm = popupCard.querySelector('.popup__form');
 const placeName = popupCard.querySelector('.popup__input_type_card-name');
 const link = popupCard.querySelector('.popup__input_type_url');
 
+const popupProfileAddButton = document.querySelector('.profile__edit-button');
+const popupProfile = document.querySelector('.popup_type_edit');
+const popupProfileCloseButton = popupProfile.querySelector('.popup__close');
 
 
 // @todo: Вывести карточки на страницу
 initialCards.forEach((cardData) => {
-    const cardElement = createCard(cardData);
-    cardList.append(cardElement);
-});
-
-// @todo: Обработчик события для открытия попапа
-profileAddButton.addEventListener("click", function(){
-    openModal(popupCard);
+    const cardElement = createCard(cardData)
+    cardList.append(cardElement)
 })
 
-closePopupButton.addEventListener("click", function(){
-    closeModal(popupCard);
+// @todo: Обработчики событий для открытия/закрытия попапов
+profileAddButton.addEventListener("click", function() {
+    openModal(popupCard)
 })
 
-// @todo: Обработчик события для закрытия попапа
+closePopupButton.addEventListener("click", function() {
+    closeModal(popupCard)
+})
+
+popupCard.addEventListener("click", closePopupOnBackground)
+
+document.addEventListener("keydown", function(event) {
+    if(event.code == 'Escape') {
+        closeModal(popupCard)
+    }
+})
+
+popupProfileAddButton.addEventListener("click", function() {
+    openModal(popupProfile)
+})
+
+popupProfileCloseButton.addEventListener("click", function() {
+    closeModal(popupProfile)
+})
+
+popupProfile.addEventListener("click", closePopupOnBackground)
+
+document.addEventListener("keydown", function(event) {
+    if(event.code == 'Escape') {
+        closeModal(popupProfile)
+    }
+})
+
 
 
--- scripts/src/components/modal.js
+++ scripts/src/components/modal.js
@@ -1,12 +1,20 @@
 // @todo: Функция открытия модального окна
 export function openModal(elem) {
     elem.classList.add('popup_is-opened');
-    console.log(elem)
 }
 
 // @todo: Функция закрытия модального окна
 export function closeModal(elem, form) {
     elem.classList.remove('popup_is-opened')
-    form.resetform();
+    // form.reset();
 }
+
+export function closePopupOnBackground( {currentTarget, target} ) {
+    const popup = currentTarget
+    const isClickedOnBackground = target === popup
+    if (isClickedOnBackground) {
+        popup.classList.remove('popup_is-opened')
+    }
+    // event.target.closest(".popup_is-opened").remove();
+  };
 
